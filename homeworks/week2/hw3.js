9d6e82d70b8707a1e4016d6e4c41b588a7733255
yukirt <jkyu@gms.ndhu.edu.tw>
2020-06-16 10:54:34 +0800

    Finish homework 3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,12 @@
 function reverse(str) {
-  
+    let result = "";
+    for (i = str.length - 1 ; i >= 0; i--){
+        result = result + str[i]
+    }
+    console.log(result)
 }
 
 reverse('hello');
+reverse('yoyoyo');
+reverse('1abc2');
+reverse('1,2,3,2,1');
