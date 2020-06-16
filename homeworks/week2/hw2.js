cb6ab7822f3c5be5678774cf64bec588c220207b
yukirt <jkyu@gms.ndhu.edu.tw>
2020-06-16 10:47:03 +0800

    Finish homework 2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,8 @@
 function capitalize(str) {
-  
+    result = str[0].toUpperCase() + str.slice(1);
+    return result
 }
 
 console.log(capitalize('hello'));
+console.log(capitalize('nick'));
+console.log(capitalize(',hello'));
